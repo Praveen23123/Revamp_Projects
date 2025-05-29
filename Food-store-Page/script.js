4d7de2bbb2fd97daff69a2b7171698522791ded9
Madhan <praveen.g2312@gmail.com>
2025-05-29 21:31:11 +0530

    Food store project

---
+++ Food-store-Page/script.js
@@ -0,0 +1,15 @@
+var menu = document.getElementById("menu");
+var cross = document.getElementById("cross");
+var nav_list = document.getElementById("nav-list");
+
+menu.addEventListener("click", () => {
+  cross.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+  nav_list.classList.toggle("hidden");
+});
+
+cross.addEventListener("click", () => {
+  cross.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+  nav_list.classList.toggle("hidden");
+});
